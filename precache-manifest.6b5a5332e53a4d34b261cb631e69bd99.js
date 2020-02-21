d699b9a41d7fa2365bbfd51e1cfdea446b0a1bb9
corujasdevbr <fernando.guerra@corujasdev.com.br>
2020-02-20 22:22:39 -0300

    Updates

---
+++ precache-manifest.6b5a5332e53a4d34b261cb631e69bd99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d4ad27636e8524de697d9afeae7bb1",
+    "url": "/jogo_da_vida/index.html"
+  },
+  {
+    "revision": "8c72934e3f91a07e267c",
+    "url": "/jogo_da_vida/static/css/main.159c9da0.chunk.css"
+  },
+  {
+    "revision": "9d428f7da4688d363e6d",
+    "url": "/jogo_da_vida/static/js/2.b660ae54.chunk.js"
+  },
+  {
+    "revision": "86b4fce93093fa1e24b23e8f37d5f666",
+    "url": "/jogo_da_vida/static/js/2.b660ae54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c72934e3f91a07e267c",
+    "url": "/jogo_da_vida/static/js/main.b9cf89c8.chunk.js"
+  },
+  {
+    "revision": "251f2000d255338ed68a",
+    "url": "/jogo_da_vida/static/js/runtime-main.ef1edc20.js"
+  },
+  {
+    "revision": "48cb1c9e25365eb0f295ccd4f5b60014",
+    "url": "/jogo_da_vida/static/media/tabuleiros-curvas_analist_devops.48cb1c9e.jpg"
+  }
+]);
